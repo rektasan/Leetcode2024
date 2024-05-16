a8e720ec0fc39a4903cc2f6b6ca9381552770a8a
opopo <itsoldleg@gmail.com>
2024-05-17 02:47:33 +0600

    First Commit

---
+++ src/main/java/Problems/problem2331/Solution.java
@@ -0,0 +1,19 @@
+package Problems.problem2331;
+
+public class Solution {
+
+  public boolean evaluateTree(TreeNode root) {
+    if (root.left == null && root.right == null) {
+      return root.val == 1;
+    }
+    if (root.val == 2) {
+      assert root.left != null;
+      return evaluateTree(root.left) || evaluateTree(root.right);
+    } else if (root.val == 3) {
+      assert root.left != null;
+      return evaluateTree(root.left) && evaluateTree(root.right);
+    }
+    throw new IllegalArgumentException("Invalid node value: " + root.val);
+  }
+
+}
---
+++ src/main/java/Problems/problem2331/Test.java
@@ -0,0 +1,13 @@
+package Problems.problem2331;
+
+public class Test {
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(2);
+    root.left = new TreeNode(1);
+    root.right = new TreeNode(3, new TreeNode(0), new TreeNode(1));
+
+    Solution solution = new Solution();
+    System.out.println(solution.evaluateTree(root));
+  }
+}
